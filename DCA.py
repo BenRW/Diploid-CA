1c0393d95070ca7eda57ec5c0808476d94d331f8
Ben R-W <br2417@ic.ac.uk>
2021-10-11 12:39:34 +0200

    vary_lambda goes one step back at change and fixed mistake in get_neighbourhood

--- DCA.py
+++ DCA.py
@@ -17,11 +17,14 @@ class DiploidCA:
     def get_neighbourhood(self, i):
         # note that the boundaries are periodic
         if i>0 and i<len(self._arr)-2:
+            # if entry somewhere in middle of array
             self._nhood = self._arr[i-1:i+2]
         elif i<len(self._arr)-2:
-            self._nhood = np.array([self._arr[-1], self._arr[i], self._arr[i+1]])
+            # if entry at start of array
+            self._nhood = np.array([self._arr[-1], self._arr[0], self._arr[1]])
         else:
-            self._nhood = self._arr[i:i+2-len(self._arr)]
+            # if entry at end of array
+            self._nhood = np.array([self._arr[-2], self._arr[-1], self._arr[0]])
 
     def f1(self):
         return 0
@@ -35,7 +38,7 @@ class DiploidCA:
         
         return new_val
 
-    def iterate_dCA(self, t):
+    def iterate_dCA(self, t, history):
         new_arr = np.zeros(self._n)
 
         # generate array of length n determining whether f2 will be followed
@@ -51,11 +54,12 @@ class DiploidCA:
                 new_arr[i] = self.f1()
         
         self._arr = new_arr.copy()
-        self._history[t] = new_arr.copy()
+        if history:
+            self._history[t] = new_arr.copy()
     
-    def run(self, history = False):
+    def run(self, history=False):
         for t in range(1, self._nt):
-            self.iterate_dCA(t)
+            self.iterate_dCA(t, history)
         
         if history:
             return self._arr, self._history
@@ -68,15 +72,4 @@ class DiploidCA:
         return d
 
 
-DCA1 = DiploidCA(n=10000, nt=5000, l=0.7)
-final_arr1, history1 = DCA1.run(history=True)
 
-d1 = DCA1.get_density()
-
-print(d1)
-
-
-
-
-
-    
--- tasks.py
+++ tasks.py
@@ -1,27 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import DCA
+import time
 
 
 def vary_lambda():
     l = 0
-    threshold = 0.1
+    threshold = 0.02
     density = []
     l_array = []
     n = 0
 
     while l < 1.:
-        DCA_i = DCA.DiploidCA(n=10000, nt=5000, l=l)
+        DCA_i = DCA.DiploidCA(n=1000, nt=500, l=l)
         DCA_i.run()
         density.append(DCA_i.get_density())
         l_array.append(l)
 
-        # if n==0 or density[n] - density[n-1]>threshold:
-        #     l += 0.01
-        #     threshold = 0.2
-        # else:
-        #     l += 0.05
-        #     threshold = 0.1
+        if n==0 or density[n] - density[n-1]>threshold:
+            l += 0.01
+            # threshold = 0.2
+        else:
+            l += 0.05
+            # threshold = 0.1
+        # l += 0.05
 
         n+=1
 
@@ -32,3 +34,9 @@ def vary_lambda():
     plt.show()
 
     return l_array, density
+
+start = time.time()
+
+l, d = vary_lambda()
+
+print(time.time()-start)
