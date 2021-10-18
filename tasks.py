d9f858149dd5b451cc556001d3396b76b5cf53d1
Anne Aarts <anneaarts@live.nl>
2021-10-18 10:48:04 +0200

    implementation density change2

--- tasks.py
+++ tasks.py
@@ -6,25 +6,31 @@ import time
 
 def vary_lambda():
     l = 0
-    threshold = 0.02
+    threshold = 0.01
     density = []
     l_array = []
     n = 0
+    large_step = False
 
     while l < 1.:
-        DCA_i = DCA.DiploidCA(n=1000, nt=500, l=l)
-        DCA_i.run()
+        DCA_i = DCA.DiploidCA(n=2000, nt=1000, l=l)
+        DCA_i.run(history=False)
         density.append(DCA_i.get_density())
         l_array.append(l)
 
         if n==0 or density[n] - density[n-1]>threshold:
+            #threshold = 0.005
+            if large_step:
+                l = l_array[-2]
+                del density[-1]
+                del l_array[-1]
+                n-=1
             l += 0.01
-            # threshold = 0.2
+            large_step = False
         else:
             l += 0.05
-            # threshold = 0.1
-        # l += 0.05
-
+            #threshold = 0.01
+            large_step = True
         n+=1
 
     density = np.asarray(density)
@@ -37,6 +43,7 @@ def vary_lambda():
 
 start = time.time()
 
-l, d = vary_lambda()
+larray, density = vary_lambda()
 
-print(time.time()-start)
+speed = time.time() - start
+print('Simulation time: '+str(speed))
