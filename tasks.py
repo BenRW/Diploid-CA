9502e6c0417556b7315bb5464fbde5c2507e0820
Ben R-W <br2417@ic.ac.uk>
2021-11-03 10:13:53 +0100

    Outputted 5 more runs of the diploid CA

--- tasks.py
+++ tasks.py
@@ -101,8 +101,8 @@ def vary_lambda_analysis(size, n_iterations):
 
 # start = time.time()
 
-for _ in range(1):
-    larray, density = vary_lambda(10000, 5000)
+# for _ in range(4):
+#     larray, density = vary_lambda(10000, 5000)
 
 # speed = time.time() - start
 # print('Simulation time: '+str(speed))
